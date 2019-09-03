4fe26312b42f04305f46402200c5c60743fab441
Gaston Yelmini <gastonyelmini@gmail.com>
2019-09-03 15:24:01 -0300

    Attacha mongodb

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require("express");
-const request = require("request");
+const mongoose = require("mongoose");
 const app = express();
 
 const misc = require("./routes/api/misc");
 
 require("dotenv").config();
 
-require("request-debug")(request, function(type, data, r) {
-  console.log(
-    "**************************** NEW REQUEST ****************************"
-  );
-  console.log("TYPE: ", type);
-  console.log("TYPE: ", data);
-  console.log("TYPE: ", r);
-});
-
 app.use("/api", misc);
+
+const options = { useNewUrlParser: true };
+
+mongoose
+  .connect(process.env.MONGODB_URI, options)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log("Mongo DB conn error ", err));
 
 app.listen(process.env.PORT || 80, () =>
   console.log("listening on port " + process.env.PORT)
